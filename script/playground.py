84d3f1e4ebc03c98926c27031be98bda2e0deec7
Stefano Roncelli <stefano.roncelli@gmail.com>
2020-12-14 20:06:41 +0100

    del unused files

--- script/playground.py
+++
@@ -1,8 +0,0 @@
-
-#df = pd.read_csv('/home/stefano/PycharmProjects/lb2-2020-project-roncelli/data/training/jpred4.tsv',sep='\t', low_memory=False)
-
-#for file in os.listdir('../data/training/
-
-
-class aaaa(str)
-
