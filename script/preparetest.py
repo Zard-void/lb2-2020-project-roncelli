261c3d25ce8209f5b80d9cf24503e6d86f672921
Stefano Roncelli <stefano.roncelli@gmail.com>
2020-12-16 00:55:25 +0100

    add call external function for profile generation

--- script/preparetest.py
+++ script/preparetest.py
@@ -1,18 +1,14 @@
-import os
 from contextlib import ExitStack
 from tempfile import NamedTemporaryFile, TemporaryDirectory, gettempdir
 import biotite.application.dssp as dssp
 import biotite.structure as struc
-import biotite.structure.io.pdbx as pdbx
 import biotite.database.rcsb as rcsb
+import biotite.structure.io.mmtf as mmtf
 import pandas as pd
 from Bio import SeqIO
-from Bio.Blast.Applications import NcbipsiblastCommandline, NcbimakeblastdbCommandline
 from sultan.api import Sultan
 from tqdm import tqdm
-from joblib import dump
-from time import sleep
-
+from profile_tools import generate_profiles
 
 def filter_entries(pdb_download):
     pdb_download = pdb_download[pdb_download['Sequence'].str.contains('Sequence') == False]
@@ -73,18 +69,14 @@ if __name__ == '__main__':
     full_test = pd.DataFrame()
     list_h = ['H']
     list_e = ['E', 'B']
-
-    s.gunzip('-fk ../data/swiss-prot/uniprot_sprot.fasta.gz').run()
-    cmd = NcbimakeblastdbCommandline(input_file='../data/swiss-prot/uniprot_sprot.fasta', dbtype='prot')
-    cmd()
-
+    count = 0
     for _, sample in tqdm(clean_test.iterrows(), total=len(clean_test), desc='Generating DSSP'):
         sample_id = sample['ID']
         chain = sample['Chain']
         sequence = sample['Sequence']
-        file_name = rcsb.fetch(sample_id, "cif", gettempdir(), overwrite=False)
-        mmcif_file = pdbx.PDBxFile.read(file_name)
-        array = pdbx.get_structure(mmcif_file, model=1)
+        file_name = rcsb.fetch(sample_id, "mmtf", gettempdir())
+        mmtf_file = mmtf.MMTFFile.read(file_name)
+        array = mmtf.get_structure(mmtf_file, model=1)
         structure = array[struc.filter_amino_acids(array)]
         structure = structure[structure.chain_id == chain]
         structure.set_annotation('chain_id', ['A' for el in structure.chain_id])
@@ -96,65 +88,10 @@ if __name__ == '__main__':
             continue
         sample['Structure'] = sse
         full_test = full_test.append(sample)
-
-
+        count += 1
+        if count == 10:
+            break
     full_test.set_index(['ID', 'Chain'], inplace=True)
-    if not os.path.exists('../data/test/profile'):
-        s.mkdir('../data/test/profile').run()
+    generate_profiles(full_test, '../data/test/')
 
-    with TemporaryDirectory() as psi_temp:
-        for _, sample in tqdm(full_test.iterrows(), total=len(full_test), desc='Generating profiles'):
-            with NamedTemporaryFile(mode='w') as blast_in:
-                sequence, structure = sample[['Sequence', 'Structure']]
-                sample_id, chain = sample.name[0], sample.name[1]
-                out_name = f'{sample_id}_{chain}'
-                structure = ' ' + structure
-                print(f'>{sample_id}_{chain}', file=blast_in)
-                print(sequence, file=blast_in)
-                blast_in.seek(0)
-                cmd = NcbipsiblastCommandline(query=blast_in.name,
-                                              db='../data/swiss-prot/uniprot_sprot.fasta',
-                                              evalue=0.01,
-                                              num_iterations=3,
-                                              out_ascii_pssm=f'{psi_temp}/{out_name}.pssm',
-                                              num_descriptions=10000,
-                                              num_alignments=10000,
-                                            #  out=f'{psi_temp}{out_name}.alns.blast',
-                                              num_threads=8)
-                cmd()
 
-                if not os.path.exists(os.path.join(psi_temp, out_name + '.pssm')):
-                    tqdm.write(f'Unable to generate profile for {out_name}. No hits in the database.')
-                    full_test.drop(index=sample.name, inplace=True)
-                    continue
-                with open(f'{psi_temp}/{out_name}.pssm', 'r') as pssm_file:
-                    pssm_file.readline()
-                    pssm_file.readline()
-                    file_list = []
-                    offset = False
-                    position = 0
-                    for line in pssm_file:
-                        line = line.rstrip()
-                        if not line:
-                            break
-                        line = line.split()
-                        line.append(structure[position])
-                        position += 1
-                        if not offset:
-                            for i in range(2):
-                                line.insert(0, '')
-                                offset = True
-                        file_list.append(line)
-                    df = pd.DataFrame(file_list)
-                    df.drop((df.columns[col] for col in range(2, 22)), axis=1, inplace=True)
-                    df.drop((df.columns[-3:-1]), axis=1, inplace=True)
-                    df.drop((df.columns[0]), axis=1, inplace=True)
-                    df.columns = df.iloc[0]
-                    df = df[1:]
-                    df.rename(columns={df.columns[0]: "Sequence"}, inplace=True)
-                    df.rename(columns={df.columns[-1]: "Structure"}, inplace=True)
-                    df = df[['Structure'] + [col for col in df.columns if col != 'Structure']]
-                    df.loc[:, 'A':'V'] = df.loc[:, 'A':'V'].astype(float).divide(100)
-                    df.to_csv(f'../data/test/profile/{sample_id}_{chain}.profile', sep='\t', index=False)
-    print('Dumping clean test to data/test/full_test.joblib. Profiles are generated in data/test/profile.')
-    dump(full_test, '../data/test/full_test.joblib')
