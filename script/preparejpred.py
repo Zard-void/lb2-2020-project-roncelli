6d5f1833f07dbbc5616a14b5a7e41002d8a3823a
Stefano Roncelli <stefano.roncelli@gmail.com>
2020-12-16 14:44:07 +0100

    feat: now uses a zip file to unpack

--- script/preparejpred.py
+++ script/preparejpred.py
@@ -1,15 +1,19 @@
 import pandas as pd
 from profile_tools import generate_profiles
-
+from sultan.api import Sultan
 
 if __name__ == '__main__':
     summary = pd.read_csv('../data/training/jpred4_summary.tsv', sep='\t', header=0, index_col=0)
     jpred = pd.DataFrame(columns=['Sequence', 'Structure'])
+    s = Sultan()
+    s.unzip('-o ../data/training/jpred_dssp.zip -d /tmp').run()
+    s.unzip('-o ../data/training/jpred_fasta.zip -d /tmp').run()
+    print('Parsing fasta and dssp files')
     for _, sample in summary.iterrows():
         jpred_id = sample.name
         sample.drop(sample.index, inplace=True)
-        with open(f'../data/training/fasta/{jpred_id}.fasta') as fasta_file, \
-                open(f'../data/training/dssp/{jpred_id}.dssp') as dssp_file:
+        with open(f'/tmp/{jpred_id}.fasta') as fasta_file, \
+                open(f'/tmp/{jpred_id}.dssp') as dssp_file:
             fasta_file.readline(), dssp_file.readline()
             sequence = fasta_file.readline().rstrip()
             structure = dssp_file.readline().rstrip()
